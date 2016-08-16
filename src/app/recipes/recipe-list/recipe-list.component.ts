2a943fd46ff2bc7018fbe6ac1f3c08864f280a77
Edwin A. Trivinos R <edwin.trivinos@hotmail.com>
2016-08-16 15:58:42 -0500

    Add ingredient list using *ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,9 +10,11 @@ import { RecipeItemComponent } from './recipe-item.component';
   directives: [RecipeItemComponent]
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Dummy', 'Dummy', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRxB0tOdt9pf0gFkXU0TK2cOEioVjo2Za_MspENSadj0QcFjYmy', []),
+    new Recipe('Dummy', 'Dummy', 'http://3k8rzg6jh5t1c4xc01udrwh1.wpengine.netdna-cdn.com/wp-content/uploads/2014/06/dsc_0954.jpg', [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Dummy', 'https://pbs.twimg.com/profile_images/1620149654/avatar.jpg');
 
   constructor() {}
 
