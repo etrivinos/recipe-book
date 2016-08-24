260ed8e438492d1d7018abbad0fb097d5e56cfdb
Edwin A. Trivinos R <edwin.trivinos@hotmail.com>
2016-08-24 17:45:46 -0500

    Add the ingredients - Cross Service Communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
+
 import { Recipe } from './recipe';
+import { Ingredient } from '../shared';
 
 @Injectable()
 export class RecipeService {
 
   private recipes: Recipe[] = [
-    new Recipe('Dummy', 'Dummy', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRxB0tOdt9pf0gFkXU0TK2cOEioVjo2Za_MspENSadj0QcFjYmy', []),
+    new Recipe('Dummy', 'Dummy', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRxB0tOdt9pf0gFkXU0TK2cOEioVjo2Za_MspENSadj0QcFjYmy',
+        [
+          new Ingredient('French Fries', 2),
+          new Ingredient('Pork Meat', 1)
+        ]),
     new Recipe('Dummy', 'Dummy', 'http://3k8rzg6jh5t1c4xc01udrwh1.wpengine.netdna-cdn.com/wp-content/uploads/2014/06/dsc_0954.jpg', [])
   ];
 
